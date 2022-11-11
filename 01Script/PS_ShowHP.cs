6b07a46bb5844647c02bebcdc4c85463ad051307
Park132 <aice7@hanmail.net>
2022-11-11 21:35:37 +0800

    P_8day-1
    지점 위에 HP뜨게 함

--- 01Script/PS_ShowHP.cs
+++ 01Script/PS_ShowHP.cs
@@ -6,20 +6,23 @@ using TMPro;
 public class PS_ShowHP : MonoBehaviour
 {
     private int hp;
-    TextMeshProUGUI hp_txt;
+    public TextMeshProUGUI hp_txt;
+
+    public GameObject hp_target;
 
     SlimeBaseSC sc;
 
     private void Awake()
     {
-        hp_txt = GetComponent<TextMeshProUGUI>();
+        //hp_txt = empty1.gameObject.GetComponent<TextMeshProUGUI>();
         sc = gameObject.GetComponentInParent<SlimeBaseSC>();
     }
 
     private void Update()
     {
-        Debug.Log(sc.Health);
+        hp_target.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 0.8f, 0));
+        hp = sc.Health;
+        //Debug.Log("hp : " + hp);
         hp_txt.text = "HP : " + hp.ToString();
-        Debug.Log("hp : " + hp);
     }
 }
