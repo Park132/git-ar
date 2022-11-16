70f426a585567b6a48cae445a3743d9d548f0ebb
Park132 <aice7@hanmail.net>
2022-11-16 22:14:24 +0800

    P_12day-2
    - 타원 맵 다시 개발 중

--- 01Script/PS_MapMake.cs
+++ 01Script/PS_MapMake.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PS_MapMake : MonoBehaviour
 {
-    public GameObject wall; // 벽으로 생성될 오브젝트, 추후의 배열로 선언될 예정
+    public GameObject[] wall; // 벽으로 생성될 오브젝트, 추후의 배열로 선언될 예정
     public GameObject centerm1;
 
     public GameObject wall_marker1;
@@ -19,7 +19,7 @@ public class PS_MapMake : MonoBehaviour
         MapInit3(c1, c2, 10, 7);
     }
 
-    void MapInit3(Transform o1, Transform o2, float a, float b) // 3안
+    public void MapInit3(Transform o1, Transform o2, float a, float b) // 3안
     {
         // 물체1의 위치, 물체2의 위치, x축 가중치, z축 가중치
         // 삼각함수로 구현해내는 방안
@@ -28,8 +28,9 @@ public class PS_MapMake : MonoBehaviour
 
         for (int i = 0; i <= 360; i += 20) //19개 생성
         {
+            int index = Random.Range(0, 26);
             wall_marker1.transform.position = new Vector3(centerm1.transform.position.x + a * Mathf.Cos(i), 0, centerm1.transform.position.z + b * Mathf.Sin(i));
-            Instantiate(wall, wall_marker1.transform.position, wall_marker1.transform.rotation);
+            Instantiate(wall[index], wall_marker1.transform.position, Quaternion.Euler(Random.Range(1f, 179f), Random.Range(1f, 179f), Random.Range(1f, 179f)));
         }
     }
 }
